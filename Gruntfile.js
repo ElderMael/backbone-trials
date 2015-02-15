df89564925df93000ee3e6238d1c982cf3fa8a56
ElderMael <sftwr.mael@gmail.com>
2015-02-14 21:05:16 -0600

    Make Nyan the Default Reporter

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function (grunt) {
         mochaTest: {
             test: {
                 options: {
-                    reporter: 'spec',
+                    reporter: 'nyan',
                     captureFile: 'results.txt',
                     quiet: false,
                     clearRequireCache: false
--- test/simple_test.js
+++ test/simple_test.js
@@ -1,5 +1,32 @@
-var assert = require("assert");
+describe('Array', function() {
 
-describe('Array', function () {
-    
+    before(function() {
+        console.log("before");
+    });
+
+    after(function() {
+        console.log("after");
+    });
+
+    beforeEach(function() {
+        console.log("beforeEach");
+    });
+
+    afterEach(function() {
+        console.log("afterEach");
+    });
+
+    it("should be easy write a test", function(done) {
+        console.log("on test");
+        done();
+    });
+
+    it("should be easy write a test", function() {
+        console.log("on test");
+    });
+
+    it("should be easy write a test", function() {
+        console.log("on test");
+    });
+
 });
